c0f4a217b1b554f142f32d7dddccbdba8d213a91
Priya Patil <priya10487@aol.com>
2015-05-13 15:46:38 +0100

    Created module for ContactsDisplay and a constant for DUMMY_CONTACTS

--- lib/contact_management.rb
+++ lib/contact_management.rb
@@ -1,29 +1,26 @@
-class ContactsDisplay
-    def dummy_contacts
-        contacts = [{:first_name => "Anna", 
+module ContactsDisplay
+    DUMMY_CONTACTS= [{:first_name => "Anna", 
+        :last_name => "Smith",
+        :dob => "10.03.1989", 
+        :address => "12 Forlease Road",
+        :postcode => "SW9 0LG"},
+
+        {:first_name => "Bob", 
             :last_name => "Smith",
-            :dob => "10.03.1989", 
-            :address => "12 Forlease Road",
-            :postcode => "SW9 0LG"},
+            :dob => "02.12.1988",
+            :address => "10 Cedars Road",
+            :postcode => "KT11 1RY"},
 
-            {:first_name => "Bob", 
-                :last_name => "Smith",
-                :dob => "02.12.1988",
-                :address => "10 Cedars Road",
-                :postcode => "KT11 1RY"},
+            {:first_name => "Emma", :last_name => "Jones",
+                :dob => "12.08.1997",
+                :address => "11 Emerald Road",
+                :postcode => "EC1Y SXY"},
 
-                {:first_name => "Emma", :last_name => "Jones",
-                    :dob => "12.08.1997",
-                    :address => "11 Emerald Road",
-                    :postcode => "EC1Y SXY"},
-
-                    {:first_name => "Sam", 
-                        :last_name => "Smith",
-                        :dob => "01.11.1976", 
-                        :address => "03 Hatton Garden", 
-                        :postcode => "EC1R 6JP"}] 
-        return contacts
-    end 
+                {:first_name => "Sam", 
+                    :last_name => "Smith",
+                    :dob => "01.11.1976", 
+                    :address => "03 Hatton Garden", 
+                    :postcode => "EC1R 6JP"}] 
 end
 
 class Read
@@ -32,7 +29,7 @@ class Read
     end
     def fetch_contact
         contact_id = @input
-        return ContactsDisplay.new.dummy_contacts[contact_id-1] 
+        return ContactsDisplay::DUMMY_CONTACTS[contact_id-1] 
     end
 end
 
@@ -45,7 +42,7 @@ class Create
         @postcode = postcode
     end
     def add_contact
-        ContactsDisplay.new.dummy_contacts << {:name => "#{@first_name} #{@last_name}",:dob => "#{@dob}", :address => "#{@address}", :postcode => "#{@postcode}"}
+        ContactsDisplay::DUMMY_CONTACTS << {:name => "#{@first_name} #{@last_name}",:dob => "#{@dob}", :address => "#{@address}", :postcode => "#{@postcode}"}
     end
 end
 
@@ -74,7 +71,7 @@ class Delete
             end
         else
             @input == "N"
-            return ContactsDisplay.new.dummy_contacts
+            return ContactsDisplay::DUMMY_CONTACTS
         end
     end
 end
@@ -87,16 +84,16 @@ class InputOutput
 
     def contacts_list
         @output.puts "View a contact:\nWhich contact would you like to view?"
-        ContactsDisplay.new.dummy_contacts.each do |key,value|
-            @output.puts "      #{key}"
+        ContactsDisplay::DUMMY_CONTACTS.each do |key|
+            @output.puts "#{key[:first_name]} #{key[:last_name]}"
         end 
     end
 
     def data_input_output_view
         #if @input.gets.to_i.is_a?Integer and @input != 0
-            @output.puts Read.new(@input.gets.to_i).fetch_contact
-        #else 
-            #@output.puts "main page"
+            @output.puts  Read.new(@input.gets.to_i).fetch_contact
+        else   
+            @output.puts "main page"
         end 
     end
 
--- spec/contact_management_spec.rb
+++ spec/contact_management_spec.rb
@@ -1,41 +1,36 @@
 require "contact_management"
 describe "contact management program" do
-    before do 
-        @output = StringIO.new
-    end
-    it "shows all the contact details as soon as the page is loaded" do
-        display_contacts = ContactsDisplay.new.dummy_contacts
-        expect(display_contacts).to start_with({:first_name=>"Anna", :last_name=>"Smith", :dob=>"10.03.1989", :address=>"12 Forlease Road", :postcode=>"SW9 0LG"})
-    end
+before do 
+    @output = StringIO.new
+end
+it "shows all the contact details as soon as the page is loaded" do
+    display_contacts = ContactsDisplay::DUMMY_CONTACTS
+    expect(display_contacts).to start_with({:first_name=>"Anna", :last_name=>"Smith", :dob=>"10.03.1989", :address=>"12 Forlease Road", :postcode=>"SW9 0LG"})
+end
 
-    it "returns the second contact if 2 is entered" do 
-        read = Read.new(2)
-        expect(read.fetch_contact).to eq({:first_name=>"Bob", :last_name => "Smith", :dob=> "02.12.1988", :address=> "10 Cedars Road", :postcode=> "KT11 1RY"})
-    end
+it "returns the second contact if 2 is entered" do 
+    read = Read.new(2)
+    expect(read.fetch_contact).to eq({:first_name=>"Bob", :last_name => "Smith", :dob=> "02.12.1988", :address=> "10 Cedars Road", :postcode=> "KT11 1RY"})
+end
 
-    it "returns - main page - if a non-integer is entered" do 
-        read = Read.new("l")
-        expect(read.fetch_contact).to end_with("main page")
-    end
+it "shows contact details of Emma Jones when key 3 is pressed" do 
+    @input = StringIO.new("3")
+    @output = StringIO.new
+    find_three = InputOutput.new(@input,@output)
+    find_three.data_input_output_view
+    expect(@output.string.chomp).to start_with("{:first_name=>\"Emma\",")
+end
 
-    it "shows contact details of Emma Jones when key 3 is pressed" do 
-        @input = StringIO.new("3")
-        @output = StringIO.new
-        find_three = InputOutput.new(@input,@output)
-        find_three.data_input_output_view
-        expect(@output.string.chomp).to start_with("{:first_name=>\"Emma\",")
-    end
+it "goes back to main menu when another key is pressed" do 
+    @input = StringIO.new("l")
+    @output = StringIO.new
+    find_three = InputOutput.new(@input,@output)
+    find_three.data_input_output_view
+    expect(@output.string.chomp).to end_with("main page")
+end
 
-    it "goes back to main menu when another key is pressed" do 
-        @input = StringIO.new("l")
-        @output = StringIO.new
-        find_three = InputOutput.new(@input,@output)
-        find_three.data_input_output_view
-        expect(@output.string.chomp).to end_with("main page")
-    end
-
-    it "adds a new contact, when the relevant details are entered" do 
-        new_contact = Create.new("Sara","Evans","04.11.1988", "35 Guildford Road", "GU22 1EY")
+it "adds a new contact, when the relevant details are entered" do 
+    new_contact = Create.new("Sara","Evans","04.11.1988", "35 Guildford Road", "GU22 1EY")
         result = new_contact.add_contact
         expect(result).to include({:name => "Sara Evans",:dob => "04.11.1988",:address => "35 Guildford Road", :postcode => "GU22 1EY"})
 end
