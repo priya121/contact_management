161c8846173061d65837c0dc63bffeb53de188f9
Priya Patil <priya10487@aol.com>
2015-05-15 16:31:13 +0100

    Extract ViewScreen class

---
+++ lib/view_screen.rb
@@ -0,0 +1,21 @@
+require "dummy_contacts_display"
+
+class ViewScreen
+
+    def initialize(input, output)
+        @input = input
+        @output = output
+    end
+
+    def show
+        @output.puts "View a contact:\nWhich contact would you like to view?"
+        ContactsDisplay::DUMMY_CONTACTS.each do |key|
+            @output.puts "#{key[:first_name]} #{key[:last_name]}"
+        end 
+    end
+
+    def perform_action
+        @output.puts Read.new(@input.gets.to_i).fetch_contact
+    end
+end
+
