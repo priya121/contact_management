6a7b7f7db8ade3233663962c6a8b7c50927bae81
Priya Patil <priya10487@aol.com>
2015-05-18 14:49:18 +0100

    separated out tests for create and view interfaces

--- spec/input_output_spec.rb
+++ spec/input_output_spec.rb
@@ -21,12 +21,21 @@ describe InputOutput do
     end
 
     it "goes to the correct screen" do
+        input = StringIO.new("2")
+        output = StringIO.new("")
+        chooser = InputOutput.new(input, output, [ScreenDouble,ViewScreen])
+        chooser.show
+        expect(output.string).to include("Which contact would you like to view?")
+    end
+
+    it "goes to the create a contact page if 1 is entered" do
         input = StringIO.new("1")
         output = StringIO.new("")
         chooser = InputOutput.new(input, output, [CreateScreen,ViewScreen,ScreenDouble])
         chooser.show
         expect(output.string).to include("Create a contact")
     end
+
 
     class ScreenDouble
         def initialize(input, output)
